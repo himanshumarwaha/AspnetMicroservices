6f090d12648c4c94a51fcbb4512c84b2194a9d1f
Himanshu Kumar <himanshu.kumar@relationinsurance.com>
2021-09-05 12:15:00 -0700

    Added Data Layer

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+        }
+
+        public IMongoCollection<Product> Products => throw new NotImplementedException();
+    }
+}
